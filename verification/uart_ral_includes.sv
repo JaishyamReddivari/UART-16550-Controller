0fe53120577a696aa27cbf05252eed81655cfaf8
Jaishyam Reddy Reddivari <167150149+JaishyamReddivari@users.noreply.github.com>
2026-03-14 12:07:49 -0400

    Create uart_ral_includes.sv

---
+++ verification/uart_ral_includes.sv
@@ -0,0 +1,10 @@
+`include "uart_ral_pkg.sv"
+
+import uart_ral_pkg::*;
+
+`include "uart_reg_adapter.sv"
+`include "uart_reg_monitor.sv"
+`include "uart_ral_driver.sv"
+`include "uart_ral_env.sv"
+`include "uart_ral_sequence.sv"
+`include "uart_ral_test.sv"
